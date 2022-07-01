b761b9a94c06c31ac06514eb490ac369cd91f8c8
Vriyas Hartama <hi@vriyas.com>
2022-07-01 20:25:06 +0700

    feat: add navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '../components/NavBar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className='max-w-screen-lg m-auto mt-20'>
-      <Component {...pageProps} />
+    <div className='max-w-screen-lg m-auto'>
+      <Navbar/>
+      <div className='mt-20'>
+        <Component {...pageProps} />
+      </div>
     </div>
   )
 }
